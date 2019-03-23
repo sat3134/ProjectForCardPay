b37831f709494053c8a9a08370e846badf23fc6b
sat3134 <47438641+sat3134@users.noreply.github.com>
2019-03-23 11:19:00 +1000

    Correction

--- CardPay/Features/CardPayWithout3DSecure.feature.cs
+++ CardPay/Features/CardPayWithout3DSecure.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Test1.Features
+namespace CardPay.Features
 {
     using TechTalk.SpecFlow;
     
