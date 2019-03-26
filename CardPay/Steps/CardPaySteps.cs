4b07cdfa57e46c87bf133aac994a0203e4b36975
sat3134 <47438641+sat3134@users.noreply.github.com>
2019-03-26 22:04:35 +1000

    Added WebDriver package

--- CardPay/Steps/CardPaySteps.cs
+++ CardPay/Steps/CardPaySteps.cs
@@ -16,6 +16,7 @@ namespace ProjectForCardPay
         [Given(@"Open Payment Page")]
         public void GivenOpenPaymentPage()
         {
+            //_driver = new ChromeDriver();
             _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
             _driver.Navigate().GoToUrl("https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d");
